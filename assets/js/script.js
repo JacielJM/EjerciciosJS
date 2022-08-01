bc76ff455c3bde61ce7119ea9e601d638fad3b72
JacielJM <jacimenera@gmail.com>
2022-08-01 17:01:10 -0500

    creacion

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// alert('Hola Jaciel, todo OK');
+console.log("Estoy viiivoooo!");
+
+let nombre = prompt("¿Cómo te llamas?");
+console.log(nombre);
+
+let base = prompt("Ingrese la base del triángulo");
+let altura = prompt("Ingrese la altura del triángulo");
+
+let areaTriangulo = (base * altura) / 2;
+
+let mensaje = `El área del triángulo es: ${areaTriangulo} unidades cuadradas`;
+console.log(mensaje);
+alert(mensaje);
---
+++ assets/js/variables.js
@@ -0,0 +1,37 @@
+// Mejores prácticas con el uso de variables
+
+var a;
+var a1;
+var a_Nombre;
+var a$Direccion; // no comúnmente utilizadas;
+
+// Declaración de variables
+
+// Número
+var edad = 25;
+var peso = 98;
+
+// Booleanos
+var ciego = true;
+var hambre = false;
+var sueño = false;
+
+// String
+var nombre = "Jaciel";
+var ciudad = "Puerto Escondido";
+var nacionalidad = `Mexicano`;
+
+// Null
+var ganasDeBibir = null;
+var dineros = null;
+var briAmonestaciones = null;
+
+var x,
+	y,
+	z = 0;
+
+var xx, yy, zz;
+
+var uxo = (dox = trex = 4);
+
+// https://paste.pelishd4k.com/?v=XI
