a0865419a100b25d4794bf6e51a4af3cee00f4ac
JacielJM <jacimenera@gmail.com>
2022-08-02 11:01:08 -0500

    ejercios 1 y 2

---
+++ assets/js/badScript.js
@@ -0,0 +1,35 @@
+//  Bad practices
+var one = 1;
+var second = "1s"; // ó  var second = 2;
+
+// Why doesn't it work?
+var if_condition = false;
+
+if (true) {
+	// do something
+}
+
+// Should equal formula for wild wings
+var buffaloWings = buffalo + wings;
+
+// Null variable
+var vacio = Null;
+
+// All variables should equal 1
+var uno,
+	one,
+	wan,
+	uma,
+	um = 1;
+
+// It's broken
+console.log(".1");
+
+// Console.log para mostrar [area] de un cuadrado
+console.log(ladoa * ladob);
+
+// Should equal 4
+var four = 2 + 2;
+
+// Doesn't work for math problems
+var PI = 3.141516;
--- assets/js/variables.js
+++ assets/js/variables.js
@@ -17,8 +17,8 @@ var hambre = false;
 var sueño = false;
 
 // String
-var nombre = "Jaciel";
-var ciudad = "Puerto Escondido";
+var nombre = 'Jaciel';
+var ciudad = 'Puerto Escondido';
 var nacionalidad = `Mexicano`;
 
 // Null
@@ -34,4 +34,66 @@ var xx, yy, zz;
 
 var uxo = (dox = trex = 4);
 
-// https://paste.pelishd4k.com/?v=XI
+// carrito de compras
+
+/* getNameVar solo funciona en la consola NodeJS por el objeto global; 
+	en el navegador se debe cambiar por el objeto nodewindow */
+// function getNameVar(v) {
+// 	for (var key in global) {
+// 		if (global[key] === v) return key;
+// 	}
+// 	return false;
+// }
+
+var item, precio, descuento, categoria, cantidad;
+// let nombreV = getNameVar(item);
+console.log(`La variable 'item' es de tipo ${typeof item}`);
+console.log(global);
+
+// typeOf
+
+var a = 2;
+typeof a; //number
+var b = 'hola';
+typeof b; //string
+var c = true;
+typeof c; //boolean
+var d = null;
+typeof d; //object
+
+// isNaN() es una función que nos permite verificar si un dato
+// es o no es un número --> weird
+
+/* Ejercicio 1 */
+
+var energia,
+	hambre,
+	motivacion,
+	animo,
+	buenHumor = false;
+var sueño,
+	malHumor,
+	flojera = true;
+
+var nombre = 'Jaciel';
+var apellidoPaterno = 'Jacinto';
+var apellidoMaterno = 'Menera';
+var perro1 = 'Chester';
+var perro2 = 'Coqueto';
+var perro3 = 'Muñeca';
+var perro4 = 'Campeona';
+var perro5 = 'Higo';
+var perro6 = 'Negrita';
+var perro7 = 'Huesito';
+var gata = 'Michu';
+
+var age = 25;
+var birthDay = 2;
+var birthMonth = 3;
+var birthYear = 1997;
+var weight = 98; //kg
+var height = 175; //cm
+var bikeTravel = 6000; //km
+var sleepHours = 6;
+var savings = 10000; //MXN
+var creditCardDebt = 3083.64; //MXN
