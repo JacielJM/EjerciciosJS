85eee8d8f8a2bccf832af5ab9a7049f81708f1df
JacielJM <jacimenera@gmail.com>
2022-08-03 11:17:00 -0500

    ejercicios 1 y 2 de fns

---
+++ assets/js/ejerciciosFunciones.js
@@ -0,0 +1,26 @@
+// EJERCICIOS
+
+// EJERCICIO 1
+function idCard() {
+	let nombre = 'Jaciel';
+	let ubicacion = 'Puerto Escondido';
+	let pasatiempo = 'ciclismo';
+	return `Hola mi nombre es ${nombre}. Vivo en ${ubicacion} y disfruto el ${pasatiempo}.`;
+}
+
+console.log(idCard());
+
+// Ejercicio 2
+function calcDogAge(dogName = 'tu perro', humanYears = null) {
+	let dogEq = 7;
+	let dogYears = humanYears * dogEq;
+	return `La edad de ${dogName} es ${dogYears} años perrunos`;
+}
+
+console.log(calcDogAge('Chester', 7));
+console.log(calcDogAge());
+
+// arrow fn
+const dogAge = (dogName, humanYears) =>
+	`${dogName} tiene ${humanYears * 7} años perrunos`;
+console.log(dogAge('Wisin', 5));
