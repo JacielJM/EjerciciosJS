42d00bc1e206214311c5693842ba70d1ba13f09e
JacielJM <jacimenera@gmail.com>
2022-08-04 14:59:51 -0500

    métodos

--- assets/js/arreglosYBucles.js
+++ assets/js/arreglosYBucles.js
@@ -65,3 +65,64 @@ const paises = [
 	'Suiza',
 	'Egipto',
 ];
+
+//Ejemplo de Arreglo 2
+listaDelSuper = ['Leche', 'Papitas', 'Jabon', 'Nachos', 'Huevos'];
+console.log(listaDelSuper);
+
+console.log(`El producto en la posición 1 es: ${listaDelSuper[1]}`);
+console.log(`El producto en la posición 4 es: ${listaDelSuper[4]}`);
+console.log(`El producto en la posición 2 es: ${listaDelSuper[2]}`);
+console.log(`El producto en la posición 6 es: ${listaDelSuper[6]}\n`);
+
+for (let i = 0; i < listaPerritos.length; i++) {
+	console.log(`El perrito en la posición ${i} es: ${listaPerritos[i]}`);
+}
+
+// https://open.spotify.com/track/4H8AfSDk1cWrYiV35VCdyA?si=f13187a87d094f7d
+
+/*
+    Array asociativo ~= Objetos
+
+    Son arreglos donde cada elemento está asociado no solo con su índice, si no que también está asignado a un identificador
+
+*/
+
+const propiedadesComputadora = {
+	marca: 'Asus',
+	procesador: 'Ryzen 5 5600X',
+	ram: '16 GB',
+	almacenamiento: '1 TB',
+	tipo: 'ssd',
+	graficos: 'RTX 3090 Ti',
+};
+
+console.log(`La RAM de mi computadora es ${propiedadesComputadora.ram}`);
+
+//Ejemplo de una Red Social
+let publicacionRedSocial = {
+	nombre: 'Felipe',
+	usuario: '@felipecontenis',
+	fecha: 'Agosto',
+	likes: '5000',
+	descripcion: 'Esta es una bonita foto de lasagna',
+	ubicacion: 'Estado de Mexico',
+};
+
+console.log(
+	'Estas son las publicaciones de Agosto: ',
+	publicacionRedSocial.fecha
+);
+
+/*
+Metodos de los arrays 
+
+En los arreglos, tenemos ciertos metodos o instrucciones que nos van a permitir manipular los elemenos de este arreglo. Estos nos permiten desde agregar y eliminar elementos, hasta reordenarlos.
+
+Podemos dividir estos metodos en 3: 
+
+    - Metodos transformadores
+    - Metodos accesores
+    - Metodos repetitivos (spoiler)
+
+*/
