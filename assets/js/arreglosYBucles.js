003cdafbdb40197c5ccf3c47682957459110dd45
JacielJM <jacimenera@gmail.com>
2022-08-04 13:00:00 -0500

    primeros arrays

--- assets/js/arreglosYBucles.js
+++ assets/js/arreglosYBucles.js
@@ -4,4 +4,64 @@
 
 ARRAYS O ARREGLOS
 
+Dfn
+Un arreglo o array es un conjunto de datos que se pueden almacenar en una sola variable.
+Cuando tenemos una variable, la usamos para almacenar un solo dato. En cambio, en un arreglo, guardamos muchos datos, a manera de lista.
+
+Un array ya no es un tipo de dato primitivo, se considera un objeto porque es una estructura de datos
+
+
 */
+
+let nombre = 'Jaciel';
+
+const array = ['Jaciel', 'Nataly', 'Mariana', 'Alonso', 'Briana'];
+
+/* CREACIÓN 
+    Usando el constructor de clase
+
+*/
+
+const marcaDeColores = new Array(
+	'Mapita',
+	'Prismacolor',
+	'BlancaNieves',
+	'Faber Castell'
+);
+
+// Directamente
+
+const listaPerritos = [
+	'labrador',
+	'corgi',
+	'San Bernardo',
+	'Pitbul',
+	'Bichón Maltés',
+];
+
+console.log(listaPerritos);
+
+// 5 arreglos random
+
+const pecesFeos = ['Pez Globo', 'Pez Bruja', 'Pez Moly', 'Pez Ángel'];
+
+const comidaMexicana = ['Chilaquiles', 'Tacos', 'Tamales', 'Mole', 'Pozole'];
+
+const pasatiempos = [
+	'Escuchar música',
+	'Patinar',
+	'Correr',
+	'Dibujar',
+	'Dormir',
+];
+
+const bebidas = ['Chocomilk', 'Margaritas', 'Horchata', 'Jamaica', 'Cerveza'];
+
+const paises = [
+	'México',
+	'Alemania',
+	'Costa Rica',
+	'Brasil',
+	'Suiza',
+	'Egipto',
+];
